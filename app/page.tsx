e14baa694830866fac6e477cb5b0fc085c3c1b4c
John Winward <john.winward2@gmail.com>
2024-02-02 14:30:06 -0500

    fix background color error

--- app/page.tsx
+++ app/page.tsx
@@ -3,13 +3,13 @@ import profilePic from '/public/images/john-winward.jpeg'
 
 export default function Home() {
   return (
-    <main className="flex min-h-fit flex-col items-center p-24">
-      <div className="text-center text-white font-mono">
+    <main className="flex h-auto flex-col items-center p-24 bg-gray-950">
+      <div className="h-auto text-center text-white font-mono">
         <h1 className="text-5xl">Welcome to winward.dev!</h1>
         <br></br>
         <p>I&apos;m John Winward, and this is my portfolio website built using NextJS.</p>
       </div>
-      <div className="flex flex-wrap md:flex-nowrap md:flex-row w-full mx-auto justify-center mt-16">
+      <div className="flex flex-wrap md:flex-nowrap md:flex-row w-full mx-auto justify-center mt-16 h-auto">
         <div className="text-white items-center w-screen p-2">
           <div className="text-center w-full">
             <h2 className="text-3xl mb-8 font-mono">About Me</h2>
@@ -17,8 +17,7 @@ export default function Home() {
               East Stroudsburg University. My current coding interest is in web development, 
               which is why I decided to build this project using the NextJS framework. I hope 
               to learn more about web development through this project, and also create a way 
-              for people to learn more about me.
-            </p>
+              for people to learn more about me.</p>
           </div>
         </div>
         <div className="text-white font-mono items-center w-full p-2">
