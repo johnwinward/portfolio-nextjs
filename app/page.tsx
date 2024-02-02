55321f668e216f9119e42e06db029ece687f9f26
John Winward <john.winward2@gmail.com>
2024-02-02 14:04:50 -0500

    make homepage more responsive for mobile

--- app/page.tsx
+++ app/page.tsx
@@ -3,15 +3,15 @@ import profilePic from '/public/images/john-winward.jpeg'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center p-24">
+    <main className="flex min-h-fit flex-col items-center p-24">
       <div className="text-center text-white font-mono">
         <h1 className="text-5xl">Welcome to winward.dev!</h1>
         <br></br>
         <p>I'm John Winward, and this is my portfolio website built using NextJS.</p>
       </div>
-      <div className="flex flex-row w-full columns-2 mx-auto justify-center mt-16">
-        <div className="text-white items-center w-full">
-          <div className="text-center">
+      <div className="flex flex-wrap md:flex-nowrap md:flex-row w-full mx-auto justify-center mt-16">
+        <div className="text-white items-center w-screen p-2">
+          <div className="text-center w-full">
             <h2 className="text-3xl mb-8 font-mono">About Me</h2>
             <p className="font-sans">I'm John, a recent college graduate with a degree in computer science from 
               East Stroudsburg University. My current coding interest is in web development, 
@@ -21,7 +21,7 @@ export default function Home() {
             </p>
           </div>
         </div>
-        <div className="text-white font-mono items-center w-full">
+        <div className="text-white font-mono items-center w-full p-2">
           <div className="flex mx-auto justify-center">
             <Image src={profilePic} alt="Photo of John Winward"></Image>
           </div>
