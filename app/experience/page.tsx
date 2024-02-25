f2b58dc8e6559db690f5917836b87afd8f41485d
John Winward <john.winward2@gmail.com>
2024-02-24 23:10:17 -0500

    fix typos

--- app/experience/page.tsx
+++ app/experience/page.tsx
@@ -22,7 +22,7 @@ export default function Experience() {
                 During the summer of 2023, I was hired as an intern at NIWC Pacific in Philadelphia. This was my first 
                 role as a developer. My internship was extended part time for the fall semester of 2023 and during the winter of early 
                 2024. During the summer, my team of 3 interns, including myself, successfully created a proof of concept application from scratch using
-                our chosen framework of Electron, which uses allows a typical JavaScript/Node.js web application to become a desktop application.
+                our chosen framework of Electron, which allows a typical JavaScript/Node.js web application to become a desktop application.
                 <br></br>
                 <br></br>
                 For the two internship extensions, I got to use other technologies and frameworks. At the time of writing this, I am still working 
@@ -30,7 +30,7 @@ export default function Experience() {
             </PageSection>
 
             <PageSection sectionData={nonDevSection}>
-                Before becoming an intern, I had other jobs. During my Sophomore year of college, I was a math tutor for the university. 
+                Before becoming an intern, I had other jobs. During my sophomore year of college, I was a math tutor for the university. 
                 I even won tutor of the month one month, and there were posters of me around campus! Before that, I worked a summer at Lowe&apos;s 
                 with 4 of my friends (best summer for sure), and I worked a few months at Sears in high school right before they closed forever.
             </PageSection>
