fb8803a6888e2adb8bc9386d2b4ea2b302a2cbe3
John Winward <john.winward2@gmail.com>
2024-09-06 19:12:50 -0400

    fix yet another apostrophe error

--- app/olivia/page.tsx
+++ app/olivia/page.tsx
@@ -6,7 +6,7 @@ export default function Olivia() {
         <main>
             <h1 className="text-white text-center text-4xl mb-12 mt-12">HI OLIVIA!</h1>
 
-            <p className="text-white text-center w-full p-12 m-auto text-xl">I created this page on my website just for you. This is the surprise I mentioned (since I'm a nerd ☝️🤓)!</p>
+            <p className="text-white text-center w-full p-12 m-auto text-xl">I created this page on my website just for you. This is the surprise I mentioned (since I&apos;m a nerd ☝️🤓)!</p>
 
             <p className="text-white text-center w-full p-12 m-auto text-xl">You really are the best girlfriend ever, and if anyone else sees this page then now they know 😁</p>
 
